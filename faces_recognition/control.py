b55002684379b05e2b617dc826bc302a2bf4f0a0
Dimax1-1 <115618293+Dimax1-1@users.noreply.github.com>
2024-11-13 16:32:02 +0100

    Add files via upload

--- faces_recognition/control.py
+++ faces_recognition/control.py
@@ -1,6 +1,6 @@
 import os, sys
 
-folder_path = '/Users/edoardocrosera/Desktop/coding/face_recognition3/faces'
+folder_path = '.../face_recognition3/faces'
 
 if not os.path.exists(folder_path):
         print("La cartella delle immagini dei volti non esiste.")
